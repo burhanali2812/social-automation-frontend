a0c58a5c84d764b408ebd5b2d5a0867cf86a84c2
burhanali2812 <syedburhanali830@gmail.com>
2026-08-15 00:42:04 +0500

    Fix issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //https://social-automation-backend.vercel.app/api
 //http://localhost:5000/api
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://social-automation-backend.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
